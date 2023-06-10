9a48b5eccc455ab97be4cc71268f94e320ef84f6
zeuzh <jezisme@outlook.com>
2023-06-10 17:12:58 -0400

    set up basic homepage layout

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,8 +1,4 @@
-// ********** index.html **********
+const selector = document.querySelector(".selector");
 const choices = document.querySelector(".choices");
 const cardOne = document.querySelector(".cardOne");
 const cardTwo = document.querySelector(".cardTwo");
-
-// ********** food.html **********
-
-// ********** drink.html **********

989c0b45e7fb35420c0febef6f820265ff2ac784
zeuzh <jezisme@outlook.com>
2023-06-10 16:48:58 -0400

    setting file structure up

---
+++ assets/js/home.js
@@ -0,0 +1,8 @@
+// ********** index.html **********
+const choices = document.querySelector(".choices");
+const cardOne = document.querySelector(".cardOne");
+const cardTwo = document.querySelector(".cardTwo");
+
+// ********** food.html **********
+
+// ********** drink.html **********

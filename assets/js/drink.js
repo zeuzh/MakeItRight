cab55d608c41626746a7b21f7b0e97257fe5323b
zeuzh <jezisme@outlook.com>
2023-06-10 18:28:15 -0400

    set up drinks api to render drink data same as food one

--- assets/js/drink.js
+++ assets/js/drink.js
@@ -1,6 +1,7 @@
 // FOOD STUFF
 const drinkForm = document.querySelector(".drinkForm");
 const drinkInput = document.querySelector(".drinkInput");
+const drinkResults = document.querySelector(".drinkResults");
 
 window.onload = function () {
   fetchDrink("");
@@ -12,8 +13,39 @@ function fetchDrink(drink) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
+      renderResults(data);
     });
+}
+
+function renderResults(data) {
+  console.log(data);
+  let drinks = data.drinks;
+  let html = drinks
+    .map((data) => {
+      let ingredients = "";
+      let ingredientNum = 1;
+
+      while (data[`strIngredient${ingredientNum}`]) {
+        const ingredient = data[`strIngredient${ingredientNum}`];
+        const measure = data[`strMeasure${ingredientNum}`];
+        ingredients += `<p class="data">Ingredient ${ingredientNum}: ${measure}: ${ingredient}</p>`;
+        ingredientNum++;
+      }
+
+      return `<div class="card max-w-sm rounded overflow-hidden shadow-lg">
+      <img class="w-full" src=${data.strDrinkThumb} alt=${data.strDrink}>
+      <div class="px-6 py-4">
+        <div class="font-bold text-xl mb-2 heading">${data.strDrink}</div>
+      </div>
+      <div class="bottom px-6 pt-4 pb-2">
+      <p class="text-gray-700 text-base">
+      ${data.strInstructions}
+        </p>
+      </div>
+    </div>`;
+    })
+    .join("");
+  drinkResults.insertAdjacentHTML("afterbegin", html);
 }
 
 drinkForm.addEventListener("submit", function (event) {
--- assets/js/food.js
+++ assets/js/food.js
@@ -28,9 +28,9 @@ function renderResults(data) {
       return `<div class="card max-w-sm rounded overflow-hidden shadow-lg">
       <img class="w-full" src=${data.recipe.image} alt=${data.recipe.label}>
       <div class="px-6 py-4">
-        <div class="font-bold text-xl mb-2 heading">${data.recipe.label}</div>
+        <div class="font-bold text-xl mb-2 foodHeading">${data.recipe.label}</div>
       </div>
-      <div class="bottom px-6 pt-4 pb-2">
+      <div class="foodBottom px-6 pt-4 pb-2">
       <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"><a class="view-btn" target="_blank" href="${data.recipe.url}">View Recipe</a></span>
       <p class="text-gray-700 text-base">
           ${data.recipe.mealType[0]}

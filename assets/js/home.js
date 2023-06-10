b5f2403c525d7f6902a2ca0ec571a902c4160f1b
zeuzh <jezisme@outlook.com>
2023-06-10 17:27:40 -0400

    basic setup of drinks and foods page

--- assets/js/home.js
+++
@@ -1,4 +0,0 @@
-const selector = document.querySelector(".selector");
-const choices = document.querySelector(".choices");
-const cardOne = document.querySelector(".cardOne");
-const cardTwo = document.querySelector(".cardTwo");

7ca473a6c7967d435f195dbb24206b38ea8a6b73
zeuzh <jezisme@outlook.com>
2023-06-10 18:20:13 -0400

    food side of site done

--- assets/js/food.js
+++ assets/js/food.js
@@ -3,6 +3,7 @@ const foodForm = document.querySelector(".foodForm");
 const foodKey = "bd1d41381c9b73f213e16535f88be93a";
 const foodId = "faba4411";
 const foodInput = document.querySelector(".foodInput");
+const foodResults = document.querySelector(".foodResults");
 
 window.onload = function () {
   fetchFood("popular");
@@ -16,8 +17,29 @@ function fetchFood(food) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
+      renderResults(data);
     });
+}
+
+function renderResults(data) {
+  console.log(data);
+  let render = data.hits
+    .map((data) => {
+      return `<div class="card max-w-sm rounded overflow-hidden shadow-lg">
+      <img class="w-full" src=${data.recipe.image} alt=${data.recipe.label}>
+      <div class="px-6 py-4">
+        <div class="font-bold text-xl mb-2 heading">${data.recipe.label}</div>
+      </div>
+      <div class="bottom px-6 pt-4 pb-2">
+      <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"><a class="view-btn" target="_blank" href="${data.recipe.url}">View Recipe</a></span>
+      <p class="text-gray-700 text-base">
+          ${data.recipe.mealType[0]}
+        </p>
+      </div>
+    </div>`;
+    })
+    .join("");
+  foodResults.insertAdjacentHTML("afterbegin", render);
 }
 
 foodForm.addEventListener("submit", function (event) {
